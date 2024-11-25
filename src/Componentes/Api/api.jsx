4735d7bf88b70ddbbc09bf88c89ffe072c45c0df
Candelaria Barjollo <candebarjollo@gmail.com>
2024-11-25 17:23:22 -0300

    cambios1

--- src/Componentes/Api/api.jsx
+++ src/Componentes/Api/api.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api'
+    baseURL: 'https://final-back-1-rzpe.onrender.com/api'
 })
 
 export default api;
